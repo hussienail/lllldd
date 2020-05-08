f4c03f06d98b5978bd33c4bdb96ea5fb237e4eb1
hussienail <63175073+hussienail@users.noreply.github.com>
2020-05-08 23:54:40 +0300

    Create lk.js

---
+++ lk.js
@@ -0,0 +1,28 @@
+$(`<img src="https://a.top4top.io/p_15895y12o1.jpg" style="width:100%;margin-top: 0px;margin-bottom: 0px;" ><center><div id="asim13"><a id="asim21" href="contact.html"style="width: 63px;padding: 1px;height: 22px;" class="btn minix btn-primary">اتصل بنا</a><a id="asim21" href="style2.html" style="width: 63px;padding: 1px;height: 22px;" class="btn minix btn-primary">زخرفة</a><a id="asim21"  
+href="sub.html"style="width: 63px;padding: 1px;height: 22px;" class="btn minix btn-primary">اشتراكات</a><a id="asim21" 
+href="Shortcut.html"style="width: 63px;padding: 1px;height: 22px;" class="btn minix btn-primary">اختصارات</a><a id="asim21" 
+href="https://play.google.com/store/apps/details?id=com.chat.chatiraqona"style="width: 63px;padding: 1px;height: 22px;" class="btn minix btn-primary">حمل التطبيق</a><a id="asim21"
+href="ruless.html"style="width: 63px;padding: 1px;height: 22px;" class="btn minix btn-primary">قوانين السوابر</a></div></div><center>`).insertBefore(".nav-tabs"),$(`<div><marquee direction="right"id="as43"scrollamount="3">  آهلا وسهلا بكم في شات عــراقــنــا ● · نورت وطنك هنا نتعرف ع بعض تحت سماء عراقنا تحشيش وضحك بين شباب وبنات وجميع نجوم العرب وشلة الحنين ومقهى ليالينا ونذكر لمة اهلنا الجميلة تجمع كل الاصدقاء عرب اكراد كلنا العراق نمازح بنوتات ونغازلهم بـ احاسيس و عشق ورومانسية ونعبر بحرية في ليالي بغداد و درر العراق ونهدي الخواطر ما بيننا واصايل الكلام تحت همس و عزف الناي وهدوء المشاعر و مسك درر العرب فوجودك معنا على قلبنا غالي وغالي كـ هيل وليل مع بنات العراق وسماء عراق الرومانسية مع جمع صبايا العراق و عالم  · ● · مع تمنياتنا للجميع بأطيب الاوقات</marquee><div>`).insertBefore("div#tlogins .lonline.light.break");$(".hand.nosel.fl.uzr.border").css({margin:"1px 0",borderRadius:"0px"}),$(".checkbox label").html(`<input id="stealth" type="checkbox" value=""><i class="fa fa-eye-slash" aria-hidden="true"></i>`),$(".nav-tabs").addClass("fl").css("width","100%"),$("div#tlogins input").attr("maxlength",12),$("textarea#tbox.fl.corner.tbox.filw").attr("maxlength",100),
+$(`<link rel="stylesheet" href="stylle.css"><style>.fr.co,.fl.mini.u-co,.fa-gear:before{display: inline !important;}#asim29{height: 100px;width:100%}.ae.fa.chats.label.fa-comment.label-warning.fa-comment:before,.ae.fa.chats.label.fa-comment.label-primary.fa-comment:before{content:"الخاص"}ul.nav.nav-tabs>li>.fa-user-plus:before,ul.nav.nav-tabs>li>.fa-user:before,ul.nav.nav-tabs>li>.fa-user:before{color:#287c86}a#asim21,div#tlogins button.btn.btn-primary,label.label.label-primary.mini.fl{background-color:#287c86!important}ul.nav.nav-tabs>li>a{background-color:#287c86}ul.nav.nav-tabs>li.active>a{background-color:#287c86}.fr.borderg{background-color:#287c86!important}.loginstat{background-color:#287c86}label.fl.nosel.label.pnhead{background-color:#287c86} .u-msg.break.fl,.fl.u-msg,.uzr {-webkit-user-select: auto !important;} </style>`).insertBefore("body");
+sendpic_=sendpic;reqlikes=0;sendpic=function(){if(getuser(myid).rep<reqlikes){alert('يتطلب منك الحصول على '+reqlikes+' لايكات'); 
+return;}else{sendpic_();}}
+$(".fa-gear").text("الاعدادات ").css("width","");
+$(".dpnl").append(`<div id="mic"style="text-align: center;font-family: el messiri,FontAwesome!important;color: #610A00; height: 100%;width:100%;"class="break light tab-pane border">نتمنى لكم اجمل وقات</BR><p> للطربكه وركص شغل </p></p><iframe width="77%" height="30%" src="https://www.youtube.com/embed/OFeNwGOjaK4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></p><img src="https://g.top4top.io/p_1572nrhl21.gif"style="width:%;margin-top: 0px;"></BR></BR> </BR><center></div><center></div>`), $('#d0').append(`<label title="مقهى المحبه"href="#"onclick="$('.pnhead').text($(this).attr('title'));hl($(this),'primary');setTimeout(function(){$('#mic').scrollTop(0);},0);$('.dpnl').show();"data-toggle="tab"data-target="#mic"class="ae fa label label-primary fa-tv"> </label>`);
+_setprofile=setprofile;
+setprofile=function (){
+    if(getuser(myid).rep<5){return;}
+    return _setprofile();
+}
+
+Tsend_=Tsend;
+reqlikes=0;
+Tsend=function(){if(getuser(myid).rep<reqlikes){alert('يتطلب منك الحصول على لايكات'+reqlikes+' '); return;}else{Tsend_();}}
+
+
+setprofile_=setprofile;
+reqlikes=0;
+setprofile=function(){if(getuser(myid).rep<reqlikes){alert('يتطلب منك الحصول على لايك '+reqlikes+''); return;}else{setprofile_();}}
+
+        if (getUrlParameter('r') != null && getUrlParameter('enter') ==null) {
+  location.href='?enter&r=ad01';
+        }
