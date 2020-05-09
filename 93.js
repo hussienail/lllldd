ccc9db33e9236b41a545326991f3f6f868a31215
hussienail <63175073+hussienail@users.noreply.github.com>
2020-05-09 04:59:03 +0300

    Update and rename 98.js to 93.js

---
+++ 93.js
@@ -0,0 +1,9 @@
+														$(`<img src="https://l.top4top.io/p_1589324m21.jpg" class="fr" style="width:100%;margin-top: 1px;">
+`).insertBefore('.nav-tabs');
+if(location.protocol!='https:'){location.protocol='https:';}
+
+"https:"!=location.protocol&&(location.protocol="https:"),$(`<img style="width:400px;margin-bottom: 298px;" ><center><div id="asim13"><a id="asim21" href="contact.html"style="width: 62px;padding:0;height: 20px;" class="btn minix btn-primary">اتصل بنا</a><a id="asim21" href="mobile.html" style="width: 62px;padding:0;height: 20px;" class="btn minix btn-primary">التطبيق</a><a id="asim21" href="rules.html"style="width: 62px;padding:0;height: 20px;" class="btn minix btn-primary">القوانين</a><a id="asim21" href="sub.html"style="width: 62px;padding:0;height: 20px;" class="btn minix btn-primary">اشتراكات</a><a id="asim21" href="https://m7s7.com/"style="width: 85px;padding:0;height: 20px;" class="btn minix btn-primary">دردشة عراقية</a><a id="asim21" href="radioiraqna.html"style="width: 50px;padding:0;height: 20px;" class="btn minix btn-primary">راديو</a><a id="asim25"></a><a id="asim27"></a></div></div><center>`).insertBefore(".nav-tabs"),$(".nav-tabs").addClass("fl").css("width","100%"),$(".tc span[class=\"fa fl\"]").addClass("fa-check");var nopm=!0,nonot=!0;$(".hand.nosel.fl.uzr.border").css({margin:"1px 0",borderRadius:"0px"}),$('.checkbox label').html(`<input id="stealth" type="checkbox" value=""><i class="fa fa-eye-slash" aria-hidden="true"></i>`),
+$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
+<link rel="stylesheet" href="https://raw.githack.com/abbose/as.js/master/nn.css">
+<style>
+<style>`).insertBefore('body');
