c97ab08c99bcd40653700fb8061ff9bf53184a14
hussienail <63175073+hussienail@users.noreply.github.com>
2020-05-10 00:06:15 +0300

    Update 67.js

--- 67.js
+++ 67.js
@@ -1,70 +1,7 @@
-		try {
-						var _0x7561=["","\x67\x65\x74\x4F\x53","\x2E","\x67\x65\x74\x4F\x53\x56\x65\x72\x73\x69\x6F\x6E","\x67\x65\x74\x42\x72\x6F\x77\x73\x65\x72","\x75\x41","\u062A\u0645\x20\u062D\u0638\u0631\u0643\x20\u0645\u0646\x20\u0627\u0644\u062F\u0631\u062F\u0634\u0629","\x73\x65\x74\x49\x74\x65\x6D"];var client2= new ClientJS();var strNa=_0x7561[0]+ client2[_0x7561[1]]()+ _0x7561[2]+ client2[_0x7561[3]]()+ _0x7561[2]+ client2[_0x7561[4]]();if(localStorage[_0x7561[5]]){if(strNa!== localStorage[_0x7561[5]]){for(var i=0;i< 5000;i++){alert(_0x7561[6])}}}else {localStorage[_0x7561[7]](_0x7561[5],strNa)}
-/*if(screen&&screen.width > screen.height
- && navigator.platform !=="Win32"){for(var i = 0 ;i<=1000;i++){alert('تم حضرك')}; window.close(); }
-if(localStorage.u1=="ڗ"||localStorage.u1=="ڙ"||localStorage.u1=="ب"){for(var i = 0 ;i<=1000;i++){alert('تم حضرك')}; window.close(); }
-*/
-$('div#l1 button').click(function(){if($('input#u1').val()==="ڗ"||$('input#u1').val()==="ز"||$('input#u1').val()==="ڙ"){window.close()}})
-$('.loginstat').removeClass('fl').addClass('fr').css({"margin-right":"2px","margin-left":"-12px"}).appendTo('a.label.label-primary.fl')
-$('.badgex').insertBefore('.nav-tabs').css({"position": "absolute","margin": "auto auto","right": "0","left": "-75px","display": "block","float": "right","width": "40px","top": "58px"})
-$(`	<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera" />
-<style>
-.lonline>.hand.nosel.fl.uzr.border>div>div:first-child {
-    max-height: 28px;
-}
-.uzr.fl.corner.borderg.mm {
-    background-color: #ffffff3b!important;
-}
-.lonline>.hand.nosel.fl.uzr.border>div {
-    width: 82%!important;
-}
-.lonline>.hand.nosel.fl.uzr.border>div>.fl.mini.u-msg {
-    text-align: center;
-    background-color: #f3f4f4;
-}
-.lonline>.hand.nosel.fl.uzr.border {
-    margin: 0px 1px 1px 2px!important;
-    padding: 1px;
-    border: none;
-    border-left: 1px solid #262161;
-    border-right: 1px solid #262161;
-    border-radius: 3px!important;
-}
-			ul.nav.nav-tabs li a {
-    width: 100%;
-    text-align: center;
-}
-			ul.nav.nav-tabs li {
-    width: 33%;
-}
-div#tlogins>a {
-    font-family: 'jazeera-light',FontAwesome;
-}
-div#tlogins .fr.borderg{
-display: none;
-}
-div#tlogins .ssss,div#tlogins .fa.fa-refresh {
-    border-radius: 0;
-}
-div#tlogins label.label.label-primary.mini.fl {
-    display: none;
-}
-div#tlogins a.label.label-primary.fl img {
-    border-radius: 12%;
-    border: 1px solid #fff;
-margin-top: -2px;
-}
-input#u3,input#u2 {
-    margin-left: 2px;
-}
-div#tlogins div#l1 button ,div#tlogins div#l2 button,div#tlogins div#l3 button {
-    width: 50px;
-}
-		</style>
 <div class="rainbow"><img src="pner1.png" class="fl" style="width:100%;margin-top: 1px;background-color: white;border-radius: 10px;"></div><a href="/contact" target="_blank" style="padding: 2px 0;width: 24%;font-size: 14px!important;border-radius: 0 15px;margin-left: 2px;color: #e50000;border-color: #e55e00 #e50000 #a00000 #e56600;" type="a" class="btn btn-default">اتصل بنا<i style="margin-left: 2px;" class="fa fa-star-o"></i></a>
-<a href="/agents" target="_blank" type="a" style="padding: 2px 0;width: 24%;font-size: 14px!important;color: #004a7f;border-color: #004a7f #036284 #0ab9df #004a7fa1;" class="btn btn-default">شتراكات<i style="margin-left: 2px;" class="fa fa-heart-o"></i></a>
-<a href="/offers" target="_blank" type="a" style="padding: 2px 0;width: 24%;font-size: 14px!important;color: #e50000;border-color: #e55e00 #e50000 #a00000 #e56600;" class="btn btn-default">قوانين<i style="margin-left: 2px;" class="fa fa-eye"></i></a>
-<a href="/services" target="_blank" style="padding: 2px 0;width: 24%;font-size: 14px!important;margin-right: -5px;border-radius: 15px 0;border-color: #004a7f #036284 #0ab9df #004a7fa1;color: #004a7f;" type="a" class="btn btn-default">تطبيق<i style="margin-left: 2px;" class="fa fa-comments-o"></i></a>
+<a href="/agents" target="_blank" type="a" style="padding: 2px 0;width: 24%;font-size: 14px!important;color: #004a7f;border-color: #004a7f #036284 #0ab9df #004a7fa1;" class="btn btn-default">وكلائنا<i style="margin-left: 2px;" class="fa fa-heart-o"></i></a>
+<a href="/offers" target="_blank" type="a" style="padding: 2px 0;width: 24%;font-size: 14px!important;color: #e50000;border-color: #e55e00 #e50000 #a00000 #e56600;" class="btn btn-default">عروضنا<i style="margin-left: 2px;" class="fa fa-eye"></i></a>
+<a href="/services" target="_blank" style="padding: 2px 0;width: 24%;font-size: 14px!important;margin-right: -5px;border-radius: 15px 0;border-color: #004a7f #036284 #0ab9df #004a7fa1;color: #004a7f;" type="a" class="btn btn-default">خدماتنا<i style="margin-left: 2px;" class="fa fa-comments-o"></i></a>
 <hr class="primaryborder" style="margin: 5px 0px 5px;border-top: 2px solid;">
 
 `).insertBefore('.nav-tabs');
@@ -77,8 +14,3 @@ $('ul.nav.nav-tabs,.nav-tabs>li>a').addClass('label-primary').css({"color":"whit
 $('ul.nav.nav-tabs').css({"padding":"0px"})
 $('input#u1').css({"max-width":"80%"})
 $('.lonline.light.break').addClass('primaryborder').css({"border-top":"2px solid","padding-top":"5px"})
-
-		} catch (e) {
-			console.log(e)
-		}
-
