d4d8bd5e0fa0b3e856f1d6e3094bf2dbfd15fdf0
hussienail <63175073+hussienail@users.noreply.github.com>
2020-05-09 05:16:55 +0300

    Update and rename 98.js to 96.js

--- 96.js
+++ 96.js
@@ -7,6 +7,4 @@ $(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-l
 <link rel="stylesheet" href="https://raw.githack.com/abbose/as.js/master/nn.css">
 <style>
 <style>`).insertBefore('body');
-$(".fa-gear").text("الاعدادات ").css("width","");
-$(".dpnl").append(`<div id="mic"style="text-align: center;font-family: el messiri,FontAwesome!important;color: #610A00; height: 100%;width:100%;"class="break light tab-pane border">نتمنى لكم اجمل وقات</BR><p> للطربكه وركص شغل </p></p><iframe width="77%" height="30%" src="https://www.youtube.com/embed/OFeNwGOjaK4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></p><img src="https://g.top4top.io/p_1572nrhl21.gif"style="width:%;margin-top: 0px;"></BR></BR> </BR><center></div><center></div>`), $('#d0').append(`<label title="مقهى المحبه"href="#"onclick="$('.pnhead').text($(this).attr('title'));hl($(this),'primary');setTimeout(function(){$('#mic').scrollTop(0);},0);$('.dpnl').show();"data-toggle="tab"data-target="#mic"class="ae fa label label-primary fa-tv"> </label>`);
 
