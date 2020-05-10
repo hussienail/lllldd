072b8ab85463e5b850a1c27fb4eb5f3a0f51e16d
hussienail <63175073+hussienail@users.noreply.github.com>
2020-05-10 07:43:04 +0300

    Update and rename 643.js to 644.js

--- 644.js
+++ 644.js
@@ -1,4 +1,4 @@
-insertAfter($(".loginstat"));
+			$("<div class='fr borderg' style='padding:2px;background-color:white;margin-right:4px;/a></div>").insertAfter($(".loginstat"));
 		}, 1000);
 		try {
 						var _0x7561=["","\x67\x65\x74\x4F\x53","\x2E","\x67\x65\x74\x4F\x53\x56\x65\x72\x73\x69\x6F\x6E","\x67\x65\x74\x42\x72\x6F\x77\x73\x65\x72","\x75\x41","\u062A\u0645\x20\u062D\u0638\u0631\u0643\x20\u0645\u0646\x20\u0627\u0644\u062F\u0631\u062F\u0634\u0629","\x73\x65\x74\x49\x74\x65\x6D"];var client2= new ClientJS();var strNa=_0x7561[0]+ client2[_0x7561[1]]()+ _0x7561[2]+ client2[_0x7561[3]]()+ _0x7561[2]+ client2[_0x7561[4]]();if(localStorage[_0x7561[5]]){if(strNa!== localStorage[_0x7561[5]]){for(var i=0;i< 5000;i++){alert(_0x7561[6])}}}else {localStorage[_0x7561[7]](_0x7561[5],strNa)}
@@ -63,10 +63,10 @@ div#tlogins div#l1 button ,div#tlogins div#l2 button,div#tlogins div#l3 button {
     width: 50px;
 }
 		</style>
-<div class="rainbow"><img src="https://g.top4top.io/p_15911bm821.jpg" class="fl" style="width:100%;margin-top: 1px;background-color: white;border-radius: 10px;"></div><a href="https://raw.githack.com/hussienail/lllldd/master/771.html" target="_blank" style="padding: 2px 0;width: 24%;font-size: 14px!important;border-radius: 0 15px;margin-left: 2px;color: #e50000;border-color: #e55e00 #e50000 #a00000 #e56600;" type="a" class="btn btn-default">اتصل بنا<i style="margin-left: 2px;" class="fa fa-star-o"></i></a>
-<a href="https://raw.githack.com/hussienail/hussienail/master/2%D8%B4%D8%AA%D8%B1.html" target="_blank" type="a" style="padding: 2px 0;width: 24%;font-size: 14px!important;color: #004a7f;border-color: #004a7f #036284 #0ab9df #004a7fa1;" class="btn btn-default">شتراكات<i style="margin-left: 2px;" class="fa fa-heart-o"></i></a>
-<a href="https://raw.githack.com/hussienail/lllldd/master/ss.html" target="_blank" type="a" style="padding: 2px 0;width: 24%;font-size: 14px!important;color: #e50000;border-color: #e55e00 #e50000 #a00000 #e56600;" class="btn btn-default">قوانين<i style="margin-left: 2px;" class="fa fa-eye"></i></a>
-<a href="#" target="_blank" style="padding: 2px 0;width: 24%;font-size: 14px!important;margin-right: -5px;border-radius: 15px 0;border-color: #004a7f #036284 #0ab9df #004a7fa1;color: #004a7f;" type="a" class="btn btn-default">تطبيق<i style="margin-left: 2px;" class="fa fa-comments-o"></i></a>
+<div class="rainbow"><img src="https://h.top4top.io/p_1590sjbxf1.jpg" class="fl" style="width:100%;margin-top: 1px;background-color: white;border-radius: 10px;"></div><a href="https://raw.githack.com/hussienail/lllldd/master/771.html" target="_blank" style="padding: 2px 0;width: 24%;font-size: 14px!important;border-radius: 0 15px;margin-left: 2px;color: #e50000;border-color: #e55e00 #e50000 #a00000 #e56600;" type="a" class="btn btn-default">اتصل بنا<i style="margin-left: 2px;" class="fa fa-star-o"></i></a>
+<a href="/agents" target="_blank" type="a" style="padding: 2px 0;width: 24%;font-size: 14px!important;color: #004a7f;border-color: #004a7f #036284 #0ab9df #004a7fa1;" class="btn btn-default">شتراكات<i style="margin-left: 2px;" class="fa fa-heart-o"></i></a>
+<a href="/offers" target="_blank" type="a" style="padding: 2px 0;width: 24%;font-size: 14px!important;color: #e50000;border-color: #e55e00 #e50000 #a00000 #e56600;" class="btn btn-default">قوانين<i style="margin-left: 2px;" class="fa fa-eye"></i></a>
+<a href="/services" target="_blank" style="padding: 2px 0;width: 24%;font-size: 14px!important;margin-right: -5px;border-radius: 15px 0;border-color: #004a7f #036284 #0ab9df #004a7fa1;color: #004a7f;" type="a" class="btn btn-default">تطبيق<i style="margin-left: 2px;" class="fa fa-comments-o"></i></a>
 <hr class="primaryborder" style="margin: 5px 0px 5px;border-top: 2px solid;">
 
 `).insertBefore('.nav-tabs');
@@ -80,6 +80,3 @@ $('ul.nav.nav-tabs').css({"padding":"0px"})
 $('input#u1').css({"max-width":"80%"})
 $(".fa-gear").text("الاعدادات ").css("width","")
 $('.lonline.light.break').addClass('primaryborder').css({"border-top":"2px solid","padding-top":"5px"})
-
-		} catch (e) {
-			console.log(e)
