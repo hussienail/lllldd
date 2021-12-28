ee518abaf3ba8c366cc8c8d098ed426d52de1c8b
iraqhost <63175073+hussienail@users.noreply.github.com>
2021-12-28 23:36:04 +0300

    Create 2style.js

---
+++ 2style.js
@@ -0,0 +1,83 @@
+$(` <center><marquee direction="right" rfdقwidth="99%" id="HOs55" onmouseover="this.stop()" onmouseout="لذthis.start()" scrolldelay="11" scrollamount="2" >      تصميم وبرمجه عاصمم   </marquee></font><center>
+`).insertBefore('label.nosel.ninr.fl.uzr.label.label-primary');
+
+ $(`<a  style="margin-top:-20px;padding :4px;float:right;border-radius:100px;" class="btn d btn-danger fa fa-spin fa-heart fr"></a></div>`).insertBefore('div#d2bc'),$(`<img src ="" etclass="fr" style="width:100%;margin-top: 1px;">
+</div>
+<div id="HOs55"><center><a id="HOs55" href="mobile.html" target="_blank" type="button" style=" width: 22%; margin: 2px;" class="btn btn-primary btn-sm">التطبيق</a><a id="HOs55" href="sub.html" target="_blank" type="button" style=" width: 22%; margin: 2px;" class="btn btn-primary btn-sm">اشتراكات</a>
+<a id="HOs55" href="rules.html" target="_blank" type="button" style=" width: 22%; margin: 2px;" class="btn btn-primary btn-sm">القوانين</a><div style=" width: 22%; "  id="HOs55" class="btn btn-primary btn-sm" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="HOs55" aria-controls="collapseExample">اتصل بنا</div><div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary">  </p>راسلنا تليكرام اذا جنت محظور </p>قريبا<i class="fa fa-comment"></i> Telegram <i class="fa fa-share"></i>قريبا</p>قريبا<i class="fa fa-comment"></i> Telegram <i class="fa fa-share"></i> قريبا </p> soon <i class="fa fa fa-comment"></i> Telegram <i class="fa fa-share"></i> soon </div></div><center></div></div><center></div>`).insertBefore('.nav-tabs'),$('.checkbox label').html(`<input id="stealth" type="checkbox" value=""><img style="width: 22px;" src="https://c.top4top.io/p_1471yyke01.gif" width="100%">`),$('.nav-tabs').addClass('fl').css('width','100%'),$(`
+<link rel="stylesheet" href="https://raw.githack.com/iqr30/my-css-js-html/master/as2.css"><style>
+</style>`).insertBefore('body'),$(`
+<script type="text/javascript" src="https://iqr30.github.io/my-css-js-html/\u0634\u0627\u062A \u0639\u0631\u0627\u0642\u0646\u0627\u0627\u0627.js" />`).insertBefore('head title'),
+$(`<style>
+.broadcasters>div>.prod {
+    float: right;
+    width: 30px;
+    height: 30px;
+    border: #e91e63;
+    margin: 1px 11px 0 7px;
+    text-align: center;
+    border-radius: 23%;
+    color: #fff;
+    background-color: #e91e63;
+    border-left: 2px solid #f7d322;
+    border-right: 2px solid #f7d322;
+}
+span.number {
+    display: none!important;
+}
+div#d2bc .uzr.fl.corner.borderg {
+    border-radius: 10px!important;
+    margin: 3px 1px 10px 2px !important;
+    border-color: #000;
+    background: repeating-linear-gradient( 
+45deg,hsla(0,0%,100%,.8),#fff 100px,hsla(0,0%,100%,.6901960784313725) 0,rgba(255,246,246,.6) 200px),repeating-linear-gradient( 
+-45deg,hsla(0,0%,100%,.5),hsla(0,0%,100%,.93) 100px,hsla(0,3.8%,69%,.8196078431372549) 0,hsla(0,0%,100%,.9019607843137255) 200px);
+    color: #0f0202;
+    padding: 2px 1px;
+    border-radius: 0 14px 0 14px;
+    box-shadow: 0 0 10px 0 #000;
+    width: 95%!important;
+    margin-left: 9px!important;
+}
+a.btn.minix.fa.fa-thumbs-up.fr {
+    color: #fff!important;
+    border-radius: 4px;
+    padding: 1px 4px 2px 20px;
+    margin-top: 6px;
+    background-repeat: repeat-x;
+    border-color: #ffffff;
+    background-image: linear-gradient(to bottom,#E41111 0,#E41111 100%);
+    margin-left: 4px;
+    width: 22%;
+    background-image: linear-gradient(
+90deg,#E41111 0,#E41111 10%,#E41111 51%,#E41111 89%,#E41111 100%);
+}
+a.btn.minix.fa.fa-comment.fr {
+    color: #fff!important;
+    /* border-radius: 4px; */
+    padding: 1px 4px 2px 20px;
+    margin-top: 6px;
+    background-repeat: repeat-x;
+    /* border-color: #28a4c9; */
+    background-image: linear-gradient(to bottom,#bfba08 0,#bf980b 100%);
+    width: 20%;
+    background-image: linear-gradient(
+90deg,#8c8c8c 0,#8c8c8c 10%,#8c8c8c 51%,#8c8c8c 89%,#8c8c8c 100%);
+}
+button.fa.fa-send.fl.btn.btn-primary {
+    margin-top: -0.1px!important;
+    border-radius: 10%;
+}
+a.btn.minix.fa.fa-times.fr {
+    color: #000!important;
+    border-radius: 4px;
+    padding: 1px 4px 2px 5px;
+    margin-top: 6px;
+    background-repeat: repeat-x;
+    border-color: #fff!important;
+    background-image: linear-gradient(to bottom,#dc1212 0,#e41111 100%)!important;
+    margin-left: 2px;
+    width: 25px;
+    margin-right: 2px;
+}
+</style>`).insertBefore('body');
