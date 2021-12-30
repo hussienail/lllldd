a0cb1a7c2a942d22a449dfac7a8933f8ab59394b
iraqhost <63175073+hussienail@users.noreply.github.com>
2021-12-30 16:19:05 +0300

    Create ZOP.js

---
+++ ZOP.js
@@ -0,0 +1,431 @@
+$(`<div id="asi33">
+<a id="asim" href="" target="_blank" type="button" style="" class="btn-baqer">ط±ظ…ط²ظٹط§طھ</a>
+<a id="asim1" href="" target="_blank" type="button" style="" class="btn-baqer">ط±ظ…ط²ظٹط§طھ</a>
+<a id="asim2" href="" target="_blank" type="button" style="" class="btn-baqer">ط±ظ…ط²ظٹط§طھ</a>
+<a id="asim3" href="" target="_blank" type="button" style="" class="btn-baqer">ط±ظ…ط²ظٹط§طھ</a>
+<a id="asim4" href="" target="_blank" type="button" style="" class="btn-baqer">ط±ظ…ط²ظٹط§طھ</a>
+<a id="asim5" href="" target="_blank" type="button" style="" class="btn-baqer">ط±ظ…ط²ظٹط§طھ</a>
+<a id="asim6" href="" target="_blank" type="button" style="" class="btn-baqer">ط±ظ…ط²ظٹط§طھ</a>
+<a id="asim7" href="" target="_blank" type="button" style="" class="btn-baqer">ط§ظ„ط²ط®ط±ظپط©</a>
+`).insertBefore('.nav-tabs');
+    $(`<style>
+ul.nav.nav-tabs>li.active>a {
+    background: linear-gradient(
+90deg,#000000 0,#40404f 10%,#40404f 51%,#40404f 89%,#000000 100%);
+    border: none;
+    border-radius: 3px;
+    color: #ffc107;
+}
+ul.nav.nav-tabs>li>a {
+    color: #fff;
+    border: none;
+    text-align: center;
+    background: linear-gradient(
+90deg,#000000 0,#40404f 10%,#40404f 51%,#40404f 89%,#000000 100%);
+    border-radius: 3px;
+    display: initial;
+    width: 90%;
+    float: left;
+    padding: 0!important;
+    margin-left: 23px;
+    position: relative;
+    right: 23px;
+}
+a.fa.fa-user {
+    width: 100px !important;
+}
+ul.nav.nav-tabs>li>.fa-user-plus:before, ul.nav.nav-tabs>li>.fa-user:before, ul.nav.nav-tabs>li>.fa-user:before {
+    float: right;
+    margin-right: -24%;
+    border: 1px solid;
+    padding: 0 2px 1px;
+    border-radius: 5px;
+    width: 25px;
+    text-align: center;
+    background-color: #fff;
+    color: #282831;
+}
+div#l1 input#u1 {
+    border-radius: 15px;
+    border: 1px solid #e1e0e0;
+    margin-top: -2px;
+    text-align: center;
+    border-right: 7px solid;
+    border-left: 7px solid;
+    border-color: #000000;
+    width: 80%;
+    margin-left: 38.5px!important;
+}
+div#l1 button {
+    padding: 2px 0!important;
+    border: 2px solid #fff;
+    animation: mymove 1s infinite;
+    border-radius: 24px!important;
+    width: 60%;
+    height: 30px;
+    margin-left: 75.5px!important;
+}
+div#l1, div#l2, div#l3 {
+    border-bottom: 2px solid;
+    margin-bottom: 2px;
+    border-top: 2px solid;
+    margin-top: 1px;
+    border-color: #18181d;
+    border-radius: 15px;
+    background: border-box;
+    height: 70px;
+    margin-bottom: 8px;
+    overflow: initial;
+}
+div#l2 input#u2, div#l3 input#u3 {
+    margin-left: -6px;
+    border-radius: 15px;
+    border: 1px solid #e1e0e0;
+    text-align: center;
+    border-right: 7px solid;
+    border-left: 7px solid;
+    border-color: #0c0c0f;
+    width: 66%;
+    padding: 0!important;
+    float: right;
+    margin: 2px 5px 2px 0;
+}
+div#l2 input#pass1 {
+    width: 55%;
+    padding: 0!important;
+    float: right;
+    margin: 2px 25px 2px 0;
+    border-radius: 15px;
+    border: 1px solid #e1e0e0;
+    text-align: center;
+    border-right: 7px solid;
+    border-left: 7px solid;
+    border-color: #0c0c0f;
+}
+input#pass2 {
+    border-radius: 15px;
+    border: 1px solid #e1e0e0;
+    text-align: center;
+    border-right: 7px solid;
+    border-left: 7px solid;
+    border-color: #0c0c0f;
+    width: 55%;
+    padding: 0!important;
+    float: right;
+    margin: 2px 25px 2px 0;
+}
+div#l2 button, div#l3 button {
+    border: 1px solid #fff!important;
+    border-radius: 24px!important;
+    height: 28px;
+    width: 30%;
+}
+label.label.label-primary.mini.fl {
+    position: relative;
+    border: none!important;
+    border-radius: 0 0 0 0!important;
+    font-family: 'Changa', cursive,FontAwesome;
+    background: linear-gradient( 
+163deg,#1a90a500 0%,#1a90a500 100%)!important;
+    font-size: 12px!important;
+    margin: 0;
+    color: #fff!important;
+    text-align: center;
+    background-image: radial-gradient(circle,#40404f,#40404f,#000000)!important;
+    width: 100%!important;
+    padding: 1px;
+    margin-top: -10px;
+    border-radius: 24px!important;
+    box-shadow: 0 2px 2px 0 rgba(0,0,0,.14),0 1px 5px 0 rgba(0,0,0,.12),0 3px 1px -2px rgba(0,0,0,.2)!important;
+}
+marquee.marquee {
+    font-family: el messiri,FontAwesome!important;
+    width: 87%!important;
+    border-radius: 10px!important;
+    background: linear-gradient( 
+90deg,#000000 0,#40404f 10%,#40404f 51%,#40404f 89%,#000000 100%);
+    position: relative;
+    top: 122px;
+    left: 52px;
+    border-right: 4px solid black !important;
+    color: white !important;
+}
+label.label.label-primary.mini.fl:before {
+    content: 'MASTER HOST';
+    font-style: revert;
+}
+span.s1.fa.fa-user.label.badgex.label-as-badge.label-success {
+    background: #00ffff00;
+    margin-left: 0px;
+    position: relative;
+    left: 0px;
+}
+button.btn.fr.btn-success.fa.fa-refresh {
+    padding: 4px 5px;
+    background-color: #40404f00!important;
+    background-image: none;
+    border: 2px solid transparent;
+    margin-top: -3px;
+    border-radius: 50px;
+}
+.fr.borderg {
+    border: none;
+    margin-top: -236px;
+    position: relative;
+    top: -60px;
+    right: 44px;
+    background: #ffffff00 !important;
+    color: white;
+    font-size: 9px !important;
+}
+label.fl.label.loginstat.label-success {
+    color: #fff;
+    transform: matrix(1,0,0,1,0,3);
+    border-radius: 15px!important;
+    font-family: el messiri,FontAwesome!important;
+    top: -7px;
+    background: radial-gradient(circle,#40404f,#40404f,#000000)!important;
+    position: relative;
+    right: 1px;
+    width: 62px;
+    height: 25px;
+    margin-top: -4px;
+}
+.fr.borderg a {
+    color: white;
+    font-size: 8px !important;
+}
+
+
+a.label.label-primary.fl {
+    border-top-left-radius: 0.13rem;
+    border-top-right-radius: 0.13rem;
+    margin: 0;
+    position: relative;
+    text-align: left;
+    font-size: 15px !IMPORTANT;
+    position: relative;
+}
+input#stealth:before {
+    content: "\f070" !IMPORTANT;
+    position: absolute;
+    right: -25px;
+    font-family: 'FontAwesome';
+    margin-top: -52px;
+    top: 50px;
+    color: red;
+}
+.checkbox input[type=checkbox], .checkbox-inline input[type=checkbox], .radio input[type=radio], .radio-inline input[type=radio] {
+    position: absolute;
+    margin-left: 6px;
+    margin-top: -48px;
+    color: transparent;
+}
+
+.checkbox label, .radio label {
+    min-height: 20px;
+    padding-left: 20px;
+    margin-bottom: 0;
+    font-weight: 400;
+    cursor: pointer;
+    color: transparent !IMPORTANT;
+}
+div#tlogins {
+    background-color: #40404f !important;
+}
+a#asim, a#asim1, a#asim2, a#asim3, a#asim4, a#asim5, a#asim6, a#asim7 {
+    font-family: el messiri,FontAwesome!important;
+    position: relative;
+    top: -2px;
+    color: #fff;
+    display: inline-block;
+    border: 0.5px solid#ffffff6e!important;
+    font-size: small;
+    padding: 0 2px;
+    margin-left: 2px;
+    border-radius: 4px;
+    border: #8e210b00;
+    border-right: 8px #000000 solid!important;
+    border-right-style: double!important;
+    border-left: 9px #000000 solid!important;
+    border-left-style: double!important;
+    background-color: #40404f!important;
+    text-align: center;
+    margin-top: 3px;
+    text-decoration: none;
+    left: 4px;
+}
+.btn-asim {
+    width: 23%;
+    height: 21px;
+}
+div#asi33 {
+    background: #40404f;
+    margin-bottom: 13px;
+    height: 160px !important;
+}
+div.hand.corner {
+    -webkit-animation: swoopOut 1s ease forwards;
+    background: linear-gradient( 
+90deg ,#5d3706 0,#5f4813 10%,#262206 51%,rgb(65,45,17) 89%,#000000 100%) !important;
+    text-align: center;
+    color: #ffffff;
+    min-width: 180px !important;
+    max-width: 260px !IMPORTANT;
+    border: 1px solid #000 !IMPORTANT;
+    z-index: 2119 !important;
+    position: absolute !IMPORTANT;
+    top: 30% !important;
+    padding: 5px !important;
+    border-radius: 5px;
+    margin-bottom: -0.2rem;
+    left: 15px;
+}
+DIV.hand.corner .borderg {
+    border: navajowhite;
+    margin: 2px;
+}
+div#isfil {
+    display: block;
+    margin-bottom: 5px;
+}
+* {
+    outline: none !important;
+}
+label.ae.label.label-primary.fa.fa-gear, label.ae.fa.label.fa-commenting-o.label-primary, label.ae.fa.label.label-primary.fa-users, label.ae.fa.chats.label.fa-comment.label-primary, label.ae.fa.label.label-primary.fa-user {
+    position: relative;
+    top: 1px;
+}
+label.ae.fa.label.label-primary.fa-user {
+    padding-right: 3px!important;
+    border-radius: 7px 0 0 15px;
+}
+label.ae.fa.chats.label.fa-comment.label-primary {
+    border-radius: 0 7px 15px 0;
+}
+label.ae.fa.label.label-primary.fa-users {
+    padding-right: 2px!important;
+    padding-left: 5px!important;
+    border-radius: 7px 0 0 15px;
+}
+label.ae.fa.label.fa-commenting-o.label-primary {
+    padding-right: 2px!important;
+    padding-right: 2px!important;
+    padding-left: 2px!important;
+    border-radius: 0 7px 15px 0;
+}
+label.ae.label.label-primary.fa.fa-gear {
+    border-radius: 7px 0 0 15px;
+}
+div#d0 {
+    background-color: #40404f!important;
+}
+div#wall .borderg {
+    width: 95% !important;
+    background: repeating-linear-gradient( 
+45deg , rgba(255, 255, 255, 0.8), rgb(255, 255, 255) 100px, #ffffffb0 0px, #fff6f699 200px), repeating-linear-gradient( 
+-45deg , rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.93) 100px, #b3adadd1 0px, #ffffffe6 200px);
+    color: #0f0202;
+    padding: 2px 1px !IMPORTANT;
+    left: 6px;
+    border-radius: 0px 14px 0px 14px !important;
+    box-shadow: 0 0 10px 0 #000;
+    margin: 6px;
+margin-left: 10px !important;
+}
+a.btn.minix.btn-primary.fa.fa-times.fl {
+    position: relative;
+    left: 252px;
+    top: 35px;
+    background: #faebd700 !important;
+    border: none;
+    color: #122b40;
+}
+a.like2.btn.minix.btn-danger.fa.fa-heart.fr {
+    animation: button2 7s ease-out infinite normal;
+    color: #ffffff;
+    background: #122b40 !IMPORTANT;
+    border-radius: 3px;
+    margin-top: 15px;
+    border: none;
+    position: relative;
+    right: 72px;
+    border-radius: 15px;
+}
+a.btn.minix.btn-primary.fa.fa-comment.fr {
+    display: block;
+    margin-top: 13px;
+    position: relative;
+    left: -231px;
+    border-radius: 15px;
+    background: #122b40 !IMPORTANT;
+    BORDER: none;
+}
+input.ispp {
+    position: relative;
+    top: 51px;
+    left: -2px;
+}
+.uzr.fl.corner.borderg.pmsgc.mm {
+    background-color: #dba3a98f !important;
+    outline: auto !important;
+    outline-offset: 0;
+    border-radius: 6px !important;
+    box-shadow: inset #007bff4f -4px 4px 12px 10px;
+}
+.btn.minix.btn-primary.fa.fa-reply.fr {
+    background: #faebd700 !important;
+    border: none;
+    color: #40404f;
+}
+.btn.minix.btn-primary.fa.fa-times.fr {
+    background: #faebd700 !important;
+    border: none;
+    color: #40404f;
+}
+.lonline.light.break {
+    position: relative;
+    top: 4px;
+}
+.broadcasters {
+    position: absolute;
+    top: 0;
+    width: 100%;
+    left: 0;
+    margin-left: 2px;
+    color: #ffffff;
+    border: 1px solid#f9f9f9!important;
+    border-radius: 0px;
+    background-image: url(https://i.imgur.com/eqGvZEC.gif)!important;
+    font-family: 'Almarai', sans-serif;
+}
+.broadcasters>div>.prod>.fa-microphone {
+    font-size: 26px !important;
+    margin-left: 0px;
+    display: block;
+    margin-top: -1px;
+    text-align: center;
+    position: relative;
+    top: 4px;
+}
+.broadcasters>div>.prod>hr {
+    margin: 0;
+    display: none;
+}
+.broadcasters>div>.prod {
+    float: right;
+    width: 45px;
+    height: 45px;
+    color: #686868;
+    border: 2px solid gainsboro;
+    margin: 0 1px;
+    text-align: center;
+    border-radius: 50%;
+    background-color: white;
+    margin-left: 6px;
+    background-image: url(https://k.top4top.io/p_1871wwqvi1.gif);
+
+}
+
+</style>`).insertBefore('body');
+$(`<marquee class="marquee" direction="right" width="100%" scrolldelay="0" scrollamount="5" style="color: #007772; background-color: #FFFFFF"> ط­ظٹط§ظƒظ… ط´ط§طھ ظ†ط¨ط¶ ط§ظ„ط¹ط±ط§ظ‚ ط§ظ„طµظˆطھظٹ</marquee>`).insertBefore('.nav-tabs');
